eacce31b7fe269720ba1d2467511635d42897fd8
vida12345 <happy123vida@gmail.com>
2025-10-24 17:44:30 +1100

    LC-increasing-triplet-subsequence [Runtime: N/A; Memory: N/A]

--- array/increasing-triplet-subsequence.java
+++ array/increasing-triplet-subsequence.java
@@ -1,17 +1,17 @@
 class Solution {
     public boolean increasingTriplet(int[] nums) {
-        if(nums.length<3)
-            return false;
-        
-        int i = 0;
-        
-        do{
-            if(nums[i] < nums[i+1] && nums[i+1] < nums[i+2])
+        int num1 = Integer.MAX_VALUE;;
+        int num2 = Integer.MAX_VALUE;;
+        for(int i = 0; i<nums.length; i++)
+        {
+            if(nums[i]<num1)
+                num1 = nums[i];
+            else if(nums[i]<num2)
+                num2 = nums[i];
+            else
                 return true;
-            i++;
-
-        }while(i<nums.length-2);
-
+        
+        }
         return false;
         
     }
